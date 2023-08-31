d1ea7d4d52aa08c932aa0900e62dca0bd44ea2b7
Carlos Hernandez <carlosllaocua@gmail.com>
2023-08-31 11:27:53 -0500

    Implementacion de AntecedentePenal

---
+++ src/main/java/pe/edu/upc/catchthem/controllers/AntecedentePenalController.java
@@ -0,0 +1,42 @@
+package pe.edu.upc.catchthem.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.catchthem.dtos.AntecedentePenalDTO;
+import pe.edu.upc.catchthem.entities.AntecedentePenal;
+import pe.edu.upc.catchthem.serviceInterfaces.IAntecedentePenalService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/antecedentes")
+public class AntecedentePenalController {
+
+    @Autowired
+    //aS <-> antecedentePenalService
+    private IAntecedentePenalService aS;
+
+    @PostMapping
+    public void registrar(@RequestBody AntecedentePenalDTO antecedentePenalDTO){
+        ModelMapper m= new ModelMapper();
+        AntecedentePenal ap = m.map(antecedentePenalDTO,AntecedentePenal.class);
+        aS.insertar(ap);
+    }
+    @GetMapping
+    public List<AntecedentePenalDTO> listar(){
+        return aS.listar().stream().map(x->{
+            ModelMapper m = new ModelMapper();
+            return m.map(x,AntecedentePenalDTO.class);
+        }).collect(Collectors.toList());
+
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id){
+        aS.eliminar(id);
+    }
+
+
+
+}
