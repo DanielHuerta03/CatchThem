986e4d3a5d2fbd2348b4065bed3de786a479c1c7
Sebastian Enrique Yon Alegre <u202113053@upc.edu.pe>
2023-09-05 18:00:42 -0500

    Eliminar SospechosoByEntidad

--- src/main/java/pe/edu/upc/catchthem/repositories/ISospechosoRepository.java
+++ src/main/java/pe/edu/upc/catchthem/repositories/ISospechosoRepository.java
@@ -2,6 +2,7 @@ package pe.edu.upc.catchthem.repositories;
 
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.catchthem.entities.Entidad;
 import pe.edu.upc.catchthem.entities.Sospechoso;
@@ -12,10 +13,6 @@ import java.util.List;
 @Repository
 public interface ISospechosoRepository extends JpaRepository<Sospechoso,Integer> {
 
-
-    //mostrar sospechoso para cada entidad (no se si va... sería más general)
-    List<Sospechoso>findSospechosoByEntidad(Integer idEntidad);
-
     //HU30
     List<Sospechoso>findSospechosoByFecha_registro(LocalDate fecha);
 
@@ -24,10 +21,6 @@ public interface ISospechosoRepository extends JpaRepository<Sospechoso,Integer>
 
     //puede ser nuevo HU
     //List<Sospechoso>findSospechosoByFecha_registroBetween(LocalDate fechainicio, LocalDate fechafinal);
-
-
-
-
 
 
 }
