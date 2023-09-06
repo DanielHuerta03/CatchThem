2366eaf87e13c66049e98f24ca4bc8be15a37a78
Carlos Hernandez <carlosllaocua@gmail.com>
2023-09-06 14:45:44 -0500

    Implementacion de Alerta

---
+++ src/main/java/pe/edu/upc/catchthem/serviceInterfaces/IAlertaService.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.catchthem.serviceInterfaces;
+
+import pe.edu.upc.catchthem.entities.Alerta;
+
+import java.util.List;
+
+public interface IAlertaService {
+
+    public void ingresar(Alerta alerta);
+    public List<Alerta> listar();
+    public void eliminar(int idAlerta);
+}
