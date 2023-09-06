2133eb95822b7613b7b82d80f99a6ed05731416e
Joaquín Huayllasco <86326250+Joaquin0107@users.noreply.github.com>
2023-09-06 00:07:25 -0500

    Implementación de Camara

---
+++ src/main/java/pe/edu/upc/catchthem/controllers/CamaraController.java
@@ -0,0 +1,38 @@
+package pe.edu.upc.catchthem.controllers;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.catchthem.dtos.CamaraDTO;
+import pe.edu.upc.catchthem.entities.Camara;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+ @RequestMapping("/camara")
+ public class CamaraController {
+        @Autowired
+        private pe.edu.upc.catchthem.serviceinterfaces.ICamaraService cS;
+
+        @PostMapping
+        public void registrar(@RequestBody CamaraDTO camaraDTO){
+            ModelMapper m= new ModelMapper();
+            Camara c = m.map(camaraDTO,Camara.class);
+            cS.insertar(c);
+        }
+        @GetMapping
+        public List<CamaraDTO> listar(){
+            return cS.listar().stream().map(x->{
+                ModelMapper m = new ModelMapper();
+                return m.map(x,CamaraDTO.class);
+            }).collect(Collectors.toList());
+
+        }
+        @DeleteMapping("/{id}")
+        public void eliminar(@PathVariable("id") Integer id){
+            cS.eliminar(id);
+        }
+
+}
