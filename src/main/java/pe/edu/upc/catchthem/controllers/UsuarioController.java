0c6adfe4ac329551cfe092a3781177bec7bd4491
Daniel <danielhuertaarellanos.1.12@gmail.com>
2023-09-05 23:19:24 -0500

    Agregando entidades de usuarios :)

---
+++ src/main/java/pe/edu/upc/catchthem/controllers/UsuarioController.java
@@ -0,0 +1,36 @@
+package pe.edu.upc.catchthem.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.catchthem.dtos.UsuarioDTO;
+import pe.edu.upc.catchthem.entities.Usuario;
+import pe.edu.upc.catchthem.serviceInterfaces.IUsuarioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    private IUsuarioService uS;
+
+    @PostMapping
+    public void registrar(@RequestBody UsuarioDTO dto){
+        ModelMapper m = new ModelMapper();
+        Usuario u = m.map(dto,Usuario.class);
+        uS.Insert(u);
+    }
+    @DeleteMapping("/id")
+    public void delete(@PathVariable("id") Integer id) {
+        uS.eliminar(id);
+    }
+    @GetMapping
+    public List<UsuarioDTO> listar(){
+        return uS.list().stream().map(x-> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x,UsuarioDTO.class);
+        }).collect(Collectors.toList());
+    }
+}
