9dc1468841f67f1deaf7e3c2ab12bfbfcbf70631
Carlos Hernandez <carlosllaocua@gmail.com>
2023-09-06 18:59:20 -0500

    Autorizacion de usuarios a listar

--- src/main/java/pe/edu/upc/catchthem/controllers/AntecedentePenalController.java
+++ src/main/java/pe/edu/upc/catchthem/controllers/AntecedentePenalController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.catchthem.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.catchthem.dtos.AntecedentePenalDTO;
 import pe.edu.upc.catchthem.entities.AntecedentePenal;
@@ -25,6 +26,7 @@ public class AntecedentePenalController {
         aS.insertar(ap);
     }
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<AntecedentePenalDTO> listar(){
         return aS.listar().stream().map(x->{
             ModelMapper m = new ModelMapper();
