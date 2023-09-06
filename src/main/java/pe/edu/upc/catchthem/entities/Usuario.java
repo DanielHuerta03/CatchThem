367f047e38f4c7edfc59e5e739cbecaea7d39af1
DanielHuerta03 <104838742+DanielHuerta03@users.noreply.github.com>
2023-09-05 22:58:03 -0500

    Update Usuario.java

--- src/main/java/pe/edu/upc/catchthem/entities/Usuario.java
+++ src/main/java/pe/edu/upc/catchthem/entities/Usuario.java
@@ -3,7 +3,7 @@ package pe.edu.upc.catchthem.entities;
 import javax.persistence.*;
 import java.time.LocalDate;
 @Entity
-@Table(name = "University")
+@Table(name = "usuario")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
