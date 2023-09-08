054504b802bcb03ba6e456c80241eb1c7ef0a54b
Sebastian Enrique Yon Alegre <u202113053@upc.edu.pe>
2023-09-07 23:45:41 -0500

    Actualización de atributos de Usuario

--- src/main/java/pe/edu/upc/catchthem/entities/Usuario.java
+++ src/main/java/pe/edu/upc/catchthem/entities/Usuario.java
@@ -1,23 +1,47 @@
-package pe.edu.upc.catchthem.dtos;
-
-
-
-import pe.edu.upc.catchthem.entities.Entidad;
-import pe.edu.upc.catchthem.entities.Role;
+package pe.edu.upc.catchthem.entities;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
+import pe.edu.upc.catchthem.entities.Entidad;
 import java.util.List;
 
-public class UsuarioDTO {
+@Entity
+@Table(name = "Usuario")
+public class Usuario implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "nombre", length = 60, nullable = false)
     private String nombre;
+    @Column(length = 60, unique = true, nullable = false)
     private String username;
+    @Column(length = 60, nullable = false)
     private String password;
+    @Column(name = "correo", length = 60, nullable = false)
     private String correo;
+    @Column(name = "telefono", length = 9, nullable = false)
     private String telefono;
+    @ManyToOne
+    @JoinColumn(name = "id_entidad")
     private Entidad entidad;
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_usuario")
     private List<Role> roles;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nombre, String username, String password, String correo, String telefono, Entidad entidad, List<Role> roles) {
+        this.id = id;
+        this.nombre = nombre;
+        this.username = username;
+        this.password = password;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.entidad = entidad;
+        this.roles = roles;
+    }
 
     public Long getId() {
         return id;
