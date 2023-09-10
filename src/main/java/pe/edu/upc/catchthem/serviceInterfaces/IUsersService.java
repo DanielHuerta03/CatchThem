ca86c587595b21fab49dff74eb9ecd02c747c915
Sebastian Enrique Yon Alegre <u202113053@upc.edu.pe>
2023-09-10 12:19:08 -0500

    Implementación de clases e interfaces de Users

---
+++ src/main/java/pe/edu/upc/catchthem/serviceInterfaces/IUsersService.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.catchthem.serviceInterfaces;
+
+import pe.edu.upc.catchthem.entities.Users;
+
+import java.util.List;
+
+public interface IUsersService {
+    public void insert(Users users);
+    public void eliminar(Long id);
+    public List<Users> listar();
+
+}
