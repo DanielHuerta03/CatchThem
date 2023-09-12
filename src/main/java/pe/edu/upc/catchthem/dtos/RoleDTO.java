4f6bdc73398c25abe784549c307b032fdd014cb2
MoralesSil <102762790+MoralesSil@users.noreply.github.com>
2023-09-12 18:06:18 -0500

    RoleDTO

--- src/main/java/pe/edu/upc/catchthem/dtos/RoleDTO.java
+++ src/main/java/pe/edu/upc/catchthem/dtos/RoleDTO.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package pe.edu.upc.catchthem.dtos;
 
 import pe.edu.upc.catchthem.entities.Usuario;
@@ -32,3 +33,38 @@ public class RoleDTO {
         this.user = user;
     }
 }
+=======
+package pe.edu.upc.catchthem.dtos;
+
+import pe.edu.upc.catchthem.entities.Users;
+
+public class RoleDTO {
+    private long id;
+    private String rol;
+    private Users user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+}
+>>>>>>> 8a7f69c (RoleDTO)
