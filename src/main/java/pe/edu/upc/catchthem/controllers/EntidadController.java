79e41fe82a9150c6481230672cc47fecc5188c48
Daniel <danielhuertaarellanos.1.12@gmail.com>
2023-09-14 23:02:02 -0500

    IMPLEMENTANDO REPORTE DE CANTIDAD DE SOSPECHOSOS POR ENTIDAD

--- src/main/java/pe/edu/upc/catchthem/controllers/EntidadController.java
+++ src/main/java/pe/edu/upc/catchthem/controllers/EntidadController.java
@@ -6,10 +6,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.catchthem.dtos.EntidadDTO;
 import pe.edu.upc.catchthem.dtos.SospechosoDTO;
+import pe.edu.upc.catchthem.dtos.SospechosoEntidadDTO;
 import pe.edu.upc.catchthem.entities.Entidad;
 import pe.edu.upc.catchthem.entities.Sospechoso;
 import pe.edu.upc.catchthem.serviceInterfaces.IEntidadService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,4 +51,19 @@ public class EntidadController {
             return m.map(x,EntidadDTO.class);
         }).collect(Collectors.toList());
     }
+
+    @GetMapping("/cantidadsospechosos")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<SospechosoEntidadDTO> cantidaddesospechososporentidad(){
+        List<String[]> lista = iEntidadService.SospechososPorEntidad();
+        List<SospechosoEntidadDTO>listadto=new ArrayList<>();
+        for(String[] data:lista){
+            SospechosoEntidadDTO dto =  new SospechosoEntidadDTO();
+            dto.setNamesEntidad(data[0]);
+            dto.setCantidadSospechosos(Integer.parseInt(data[1]));
+            listadto.add(dto);
+        }
+        return listadto;
+    }
+
 }
