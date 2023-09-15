ba33208d09c7840b6ae795fa31da616821f5e2f7
Daniel <danielhuertaarellanos.1.12@gmail.com>
2023-09-14 23:01:30 -0500

    IMPLEMENTANDO REPORTE DE CANTIDAD DE SOSPECHOSOS POR ENTIDAD

--- src/main/java/pe/edu/upc/catchthem/repositories/IEntidadRepository.java
+++ src/main/java/pe/edu/upc/catchthem/repositories/IEntidadRepository.java
@@ -1,10 +1,16 @@
 package pe.edu.upc.catchthem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.catchthem.entities.Entidad;
+
+import java.util.List;
 
 @Repository
 public interface IEntidadRepository extends JpaRepository<Entidad,Integer> {
     void deleteByIdEntidad(Integer id);
+
+    @Query(value = "select e.nombre,count(s.id_sospechoso) from entidad e inner join sospechoso s on e.id_entidad = s.id_entidad group by e.nombre" ,nativeQuery = true)
+    public List<String[]> SospechososPorEntidad();
 }
