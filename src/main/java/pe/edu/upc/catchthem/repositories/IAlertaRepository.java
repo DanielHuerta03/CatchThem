3fb4ef378ce684e4c4d27b863fc64935d3f19ab2
MoralesSil <102762790+MoralesSil@users.noreply.github.com>
2023-09-18 13:46:39 -0500

    Query Alertas por ubicacion

--- src/main/java/pe/edu/upc/catchthem/repositories/IAlertaRepository.java
+++ src/main/java/pe/edu/upc/catchthem/repositories/IAlertaRepository.java
@@ -1,9 +1,14 @@
 package pe.edu.upc.catchthem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.catchthem.entities.Alerta;
 
 @Repository
 public interface IAlertaRepository extends JpaRepository<Alerta,Integer> {
+
+  //Buscar por ubicacion
+    @Query(value = "SELECT a.ubicacion, COUNT(a) FROM Alerta a GROUP BY a.ubicacion")
+    public List<String[]> CantidadAlertasPorUbicacion();
 }
