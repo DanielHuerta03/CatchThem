49c4b8d20c801b480e1e8400ff7a4216635ed7b0
Daniel <danielhuertaarellanos.1.12@gmail.com>
2023-09-17 21:03:51 -0500

    Controller para query ActasporPolicia

--- src/main/java/pe/edu/upc/catchthem/controllers/UsersController.java
+++ src/main/java/pe/edu/upc/catchthem/controllers/UsersController.java
@@ -4,10 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.catchthem.dtos.ActasporPoliciaDTO;
+import pe.edu.upc.catchthem.dtos.AntecedentesporSospechosoDTO;
 import pe.edu.upc.catchthem.dtos.UsersDTO;
 import pe.edu.upc.catchthem.entities.Users;
 import pe.edu.upc.catchthem.serviceInterfaces.IUsersService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,5 +39,19 @@ public class UsersController {
     @PreAuthorize("hasRole('ADMIN')")
     public void delete(@PathVariable("id") Long id) {
         uS.eliminar(id);
+    }
+
+    @GetMapping("/cantidadActasPorPolicia")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public List<ActasporPoliciaDTO> cantidadActasPorPolicia(){
+        List<String[]> lista = uS.ActasporPolicia();
+        List<ActasporPoliciaDTO>listadto=new ArrayList<>();
+        for(String[] data:lista){
+            ActasporPoliciaDTO dto =  new ActasporPoliciaDTO();
+            dto.setNamePolicia(data[0]);
+            dto.setCantidadActas(Integer.parseInt(data[1]));
+            listadto.add(dto);
+        }
+        return listadto;
     }
 }
