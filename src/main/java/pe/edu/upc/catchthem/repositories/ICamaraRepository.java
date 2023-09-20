ba16df9ca773f1bec90fd6cee613f3c951946cc0
Sebastian Enrique Yon Alegre <u202113053@upc.edu.pe>
2023-09-19 20:54:56 -0500

    Borrar metodo no utilizado

--- src/main/java/pe/edu/upc/catchthem/repositories/ICamaraRepository.java
+++ src/main/java/pe/edu/upc/catchthem/repositories/ICamaraRepository.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.catchthem.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.catchthem.entities.AntecedentePenal;
 import pe.edu.upc.catchthem.entities.Camara;
@@ -8,5 +9,5 @@ import pe.edu.upc.catchthem.entities.Camara;
 import java.util.List;
 @Repository
 public interface ICamaraRepository extends JpaRepository<Camara,Integer> {
-    List<Camara> findCamaraByEntidad(Integer id_camara);
+
 }
